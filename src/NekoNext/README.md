dad100f109ad282c384996060afc4dc4120e9c0e
MOCHIZUKI Natsune <me@natsuneko.cat>
2023-10-18 23:38:40 +0900

    chore: add a new project named NekoNext

---
+++ src/NekoNext/README.md
@@ -0,0 +1,3 @@
+# NekoNext
+
+NekoNext is a C++ framework for using CLIP STUDIO PAINT SDK. It enables developers to write plugins using easily and modern APIs.
---
+++ src/NekoNext/framework.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
+#define NOMINMAX            // Enforce using min and max function provided by STL
+
+// Windows Header Files
+#include <windows.h>
